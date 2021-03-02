116d787c00107f20293689259f0c0523bc48537f
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-03-02 18:13:42 +0900

    アソシエーション

---
+++ app/Models/Post.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+
+    protected $fillable = [
+        'user_id', 'category_id', 'content', 'title'
+    ];
+
+    public function category(){
+        return $this->belongsTo(\App\Category::class,'category_id');
+      }
+
+      public function user(){
+        return $this->belongsTo(\App\User::class,'user_id');
+      }
+
+      public function comments(){
+        return $this->hasMany(\App\Comment::class,'post_id', 'id');
+      }
+
+}
